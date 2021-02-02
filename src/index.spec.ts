0afd9012a14ede5008b196cb61abe5f631cf75e9
Lau Llobet <lau.llobet@mypulse.ai>
2021-02-02 10:58:55 +0100

    🟢🟢🟢🟢 GREEN -  - to squash

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,6 +7,17 @@ export function sum(a: number, b: number): number {
 export function sum2(a: number, b: number): number {
     return a + b;
 }
+
+export function sum2(a: number, b: number): number {
+    return a + b;
+}
+
+describe('sample test-suite', () => {
+    it('sum 1 + 3 should be 4', () => {
+        expect(sum(1, 3)).eql(4);
+    });
+});
+
 describe('sample test-suite', () => {
     it('sum 1 + 3 should be 4', () => {
         expect(sum(1, 3)).eql(4);
